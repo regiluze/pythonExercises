46b3772a0741de1b0192d4d40f45587fe1b02678
regiluze <regiluze@hotmail.com>
2020-05-23 02:45:30 +0200

    Add exercise solution

--- CountingValleys/app.py
+++ CountingValleys/app.py
@@ -8,7 +8,16 @@ import sys
 
 # Complete the countingValleys function below.
 def countingValleys(n, s):
-    pass
+    level = 0
+    valleys = 0
+    for sign in s:
+        step = 1 if sign == "U" else -1
+        level += step
+        if sign == "U" and level == 0:
+            valleys +=1
+
+    return valleys
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
